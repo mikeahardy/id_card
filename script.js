685b77fa7234db31e82e7b4f4d504d23270f7d24
Crudds <michaelhardyajr@gmail.com>
2020-02-25 20:05:52 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function idCard() {
+	var	firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var address = document.getElementById("address").value;
+	document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
+	document.getElementById("postAddress").innerHTML = address;
+	var age = document.getElementById("age").value;
+	var phoneNumber = document.getElementById("phoneNumber").value;
+	var numberArray = [];
+	numberArray.push(age, phoneNumber);
+    for (var i = 0; i <= numberArray.length; i++) {
+    	if (i <= 100) {
+    		document.getElementById("postAge").innerHTML = "Age: " + age;
+    	}
+    	else (i > 100); {
+    		document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+    	}
+
+    }
+}
